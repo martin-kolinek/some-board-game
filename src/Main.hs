55299cd29934c626df763475402ad42f4b27bb0b
Martin Kolinek <martin.kolinek@gmail.com>
2017-09-13 21:46:23 +0200

    Made it work with JSaddle Warp with static files

--- src/Main.hs
+++ src/Main.hs
@@ -11,21 +11,29 @@ import Settings.Dom
 import Reflex.Dom.Main as M
 import Reflex.Dom
 import Control.Monad.Except
-import Language.Javascript.JSaddle.Warp as W
+import Language.Javascript.JSaddle.WebSockets as W
+import Network.Wai.Handler.Warp as Net (run)
+import Network.WebSockets (defaultConnectionOptions)
+import Language.Javascript.JSaddle.Run (syncPoint)
+import Network.Wai.Middleware.Static (static)
+import Language.Javascript.JSaddle.Types (JSM)
+
+mainJsm :: JSM ()
+mainJsm = M.mainWidgetWithCss mainStyleByteString $ do
+    rec
+      let applyActionWithTryFinishTurn :: Universe -> UniverseAction -> Either String Universe
+          applyActionWithTryFinishTurn universe action = do
+            withActionApplied <- action universe
+            catchError (finishTurn withActionApplied) (const $ return withActionApplied)
+      settingsDyn <- drawSettingsIcon universeDyn
+      actions <- drawPlayers universeDyn settingsDyn
+      drawErrors universeDyn actions
+      let actionsApplied = attachWith applyActionWithTryFinishTurn (current universeDyn) actions
+          correctMoves = fmapMaybe fromRight actionsApplied
+      universeDyn <- holdDyn initialUniverse correctMoves
+    return ()
 
 main :: IO ()
-main = W.run 9000 $ M.mainWidgetWithCss mainStyleByteString $ do
--- main = mainWidgetWithCss mainStyleByteString $ do
-  rec
-    let applyActionWithTryFinishTurn :: Universe -> UniverseAction -> Either String Universe
-        applyActionWithTryFinishTurn universe action = do
-          withActionApplied <- action universe
-          catchError (finishTurn withActionApplied) (const $ return withActionApplied)
-    settingsDyn <- drawSettingsIcon universeDyn
-    actions <- drawPlayers universeDyn settingsDyn
-    drawErrors universeDyn actions
-    let actionsApplied = attachWith applyActionWithTryFinishTurn (current universeDyn) actions
-        correctMoves = fmapMaybe fromRight actionsApplied
-    universeDyn <- holdDyn initialUniverse correctMoves
-  return ()
-
+main = do
+  app <- W.jsaddleOr defaultConnectionOptions (mainJsm >> syncPoint) W.jsaddleApp
+  Net.run 9000 $ static app
