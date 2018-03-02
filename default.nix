1ff6dedb24aac9da994cfa6e0feefc6aa134ad8a
Martin Kolinek <martin.kolinek@gmail.com>
2018-03-02 23:00:56 +0100

    Initial work to support nix

---
+++ default.nix
@@ -0,0 +1,19 @@
+(import ./reflex-platform {}).project ({ pkgs, ... }: {
+  packages = {
+    some-board-game = ./. ;
+  };
+
+  shells = {
+    ghc = ["some-board-game"];
+    ghcjs = ["some-board-game"];
+  };
+  
+  overrides = self: super: {
+    some-board-game-rules = self.callCabal2nix "some-board-game-rules" (pkgs.fetchFromGitHub {
+      owner = "martin-kolinek";
+      repo = "some-board-game-rules";
+      rev = "43e31aaa160f952cb99beb9d5074b6678f8b7d8c";
+      sha256 = "1w6h3hfzcg6kxdxdyfks3id0h8mwi81jshakx0rc4mibp2wpdgnn";
+    }) {};
+  };
+})
